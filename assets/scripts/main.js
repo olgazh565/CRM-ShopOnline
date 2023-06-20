ccb964f74b4c907230af22c42715729dc25e8cac
olgazh565 <olgazh565@gmail.com>
2023-06-20 16:59:04 +0300

    task complete

---
+++ assets/scripts/main.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const modalTitle = document.querySelector('.modal__title');
+const modalCloseBtn = document.querySelector('.modal__close-btn');
+const modalGoodsId = document.querySelector('.modal__id-number');
+const modalForm = document.querySelector('.modal__form');
+const modalDiscountCheckbox = document.querySelector('.form__checkbox');
+const modalDiscountNumber = document.querySelector('#discount');
+const modalTotalCount = document.querySelector('.form__total-count');
+
+
